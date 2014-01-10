e541954bba4afa30db2b7fdc39be47b3abc9d3c4
superkirbz13 <kirbz@alum.mit.edu>
2014-01-10 01:32:21 -0500

    committing version 1 of ColorRemote
    initial commit of source code

---
+++ gen/kirby/apps/colorremote/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package kirby.apps.colorremote;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/kirby/apps/colorremote/AboutDialog.java
@@ -0,0 +1,28 @@
+package kirby.apps.colorremote;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+public class AboutDialog extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+    	AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.about, null))
+               .setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                   public void onClick(DialogInterface dialog, int id) {
+                       // User cancelled the dialog
+                   }
+               });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
